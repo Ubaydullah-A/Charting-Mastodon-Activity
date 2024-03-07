46db8dc59f60fbd81547d1c9217f81d91610d6a5
Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
2024-03-07 11:36:08 +0000

    Rewrite front-end
    The front-end has been rewritten to avoid using PySimpleGUI.
    
    Resolves: #3, #10

--- front-end.py
+++ front-end.py
@@ -1,18 +1,24 @@
-import PySimpleGUI as sg
 import pickle
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 import pandas as pd
+import tkinter as tk
+from sys import exit
 
 matplotlib.use("TkAgg")
 
-def draw_figure(canvas, figure):
+def draw_figure(figure, canvas):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
     return figure_canvas_agg
+
+def on_closing():
+    if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
+        root.destroy()
+        exit()
 
 # Get collected data
 file_check = open("data", "a")
@@ -31,12 +37,17 @@ data_df = data_df.sort_values(by=['week'], ascending = False)
 data_df = data_df.reset_index()
 data_df = data_df.drop('index', axis=1)
 
+root = tk.Tk()
+root.title("Charting Mastodon Activity")
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
 # Plot graph
 x = np.arange(len(data_df))
 bar_width = 0.5
 weeks = []
 
-fig, ax = plt.subplots(num='Test graph')
+fig, ax = plt.subplots(num='Activity graph')
 
 ax.grid(axis = 'y')
 
@@ -50,24 +61,6 @@ ax.set_xticks(x*2 + bar_width)
 ax.legend(loc="right")
 ax.set_xticklabels(weeks, fontsize=5, rotation=45)
 
-# Show the graph on the window
-layout = [
-    [sg.Text("Weekly Activity")],
-    [sg.Canvas(key="-CANVAS-")],
-    [sg.Button("Close")],
-]
+draw_figure(fig, root)
 
-window = sg.Window(
-    "Matplotlib Single Graph",
-    layout,
-    location=(0, 0),
-    finalize=True,
-    element_justification="center",
-    font="Helvetica 18",
-)
-
-draw_figure(window["-CANVAS-"].TKCanvas, fig)
-
-event, values = window.read()
-
-window.close()
+root.mainloop()
