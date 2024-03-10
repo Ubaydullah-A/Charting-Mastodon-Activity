19894a685170bec92936510007bb36492464f0ee
Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
2024-03-10 15:41:33 +0000

    Rename back-end variables
    Some variables used in the back-end had ambiguous names, and so have
    been renamed appropriately. A comment has also been included at the
    start of the program to explain what it does and how to run it.

--- back-end.py
+++ back-end.py
@@ -1,3 +1,10 @@
+"""
+This program collects the appropriate activity data from a Mastodon instance.
+It is intended to run uninterrupted on a device connected to the internet.
+
+To run this, use: python3 back-end.py
+"""
+
 from requests import get
 from pickle import load, dump
 from datetime import datetime
@@ -19,9 +26,9 @@ data_file.close()
 instance = input("Please enter the URL of the instance you would like to monitor (such as https://mastodon.social/): ")
 
 # Get new data via the API
-r = ""
+requested_data = ""
 try:
-    r = get(instance + 'api/v1/instance/activity').json()
+    requested_data = get(instance + 'api/v1/instance/activity').json()
 except:
     print("Unable to connect to the instance.\nPlease ensure that the URL is correct (including ending with '/'), that the instance is **NOT** in whitelist mode, and that you are connected to the internet.")
     exit(1)
@@ -31,10 +38,10 @@ unix_timestamp = datetime.timestamp(datetime.now())
 while True:
     failed_to_collect = False
     # Get new data via the API
-    r = ""
+    requested_data = ""
     print("Attempting to collect activity data.\nPlease do NOT terminate during this process.")
     try:
-        r = get(instance + 'api/v1/instance/activity').json()
+        requested_data = get(instance + 'api/v1/instance/activity').json()
     except:
         print("Unable to collect activity data:", datetime.now())
         failed_to_collect = True
@@ -42,25 +49,25 @@ while True:
     if not failed_to_collect:
         # Format data to avoid duplicate days
         exists = False
-        for x in range(1, len(r)):
-            r[x]['week'] = datetime.fromtimestamp(int(r[x]['week'])).strftime("%d/%m/%Y")
-            temp = r[x]['week'].split("/")
-            r[x]['week'] = int(datetime.timestamp(datetime(int(temp[2]), int(temp[1]), int(temp[0]))))
-            for y in range(0, len(data)):
-                if r[x]['week'] == data[y]['week']:
+        for requested_data_entry in range(1, len(requested_data)):
+            requested_data[requested_data_entry]['week'] = datetime.fromtimestamp(int(requested_data[requested_data_entry]['week'])).strftime("%d/%m/%Y")
+            temp = requested_data[requested_data_entry]['week'].split("/")
+            requested_data[requested_data_entry]['week'] = int(datetime.timestamp(datetime(int(temp[2]), int(temp[1]), int(temp[0]))))
+            for data_entry in range(0, len(data)):
+                if requested_data[requested_data_entry]['week'] == data[data_entry]['week']:
                     exists = True
                     break
             if not exists:
                 # Add a "count" key == 1
-                r[x]['count'] = '1'
-                data.append(r[x])
+                requested_data[requested_data_entry]['count'] = '1'
+                data.append(requested_data[requested_data_entry])
             else:
                 # Sum old and new "statuses", "logins" and "registrations", and increment "count"
                 # This is used to handle data inconsistencies between requests
-                data[y]['statuses'] = str(int(data[y]['statuses']) + int(r[x]['statuses']))
-                data[y]['logins'] = str(int(data[y]['logins']) + int(r[x]['logins']))
-                data[y]['registrations'] = str(int(data[y]['registrations']) + int(r[x]['registrations']))
-                data[y]['count'] = str(int(data[y]['count']) + 1)
+                data[data_entry]['statuses'] = str(int(data[data_entry]['statuses']) + int(requested_data[requested_data_entry]['statuses']))
+                data[data_entry]['logins'] = str(int(data[data_entry]['logins']) + int(requested_data[requested_data_entry]['logins']))
+                data[data_entry]['registrations'] = str(int(data[data_entry]['registrations']) + int(requested_data[requested_data_entry]['registrations']))
+                data[data_entry]['count'] = str(int(data[data_entry]['count']) + 1)
                 continue
 
         # Store new data
