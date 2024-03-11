83f7bd405ee8f6cd828dac7e8ff566a4e6d9b6b4
Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
2024-03-11 16:06:27 +0000

    Improve back-end program consistency
    Replaced instances of double quotes with single quotes to match the
    rest of the program and the front-end program.

--- back-end.py
+++ back-end.py
@@ -1,9 +1,9 @@
-"""
+'''
 This program collects the appropriate activity data from a Mastodon instance.
 It is intended to run uninterrupted on a device connected to the internet.
 
 To run this, use: python3 back-end.py
-"""
+'''
 
 from requests import get, exceptions
 from pickle import load, dump
@@ -12,42 +12,42 @@ from sys import exit
 from time import sleep
 
 # Get the previously collected data.
-file_check = open("data", "a")
+file_check = open('data', 'a')
 file_check.close()
 data = []
 try:
-    data_file = open("data", "rb")
+    data_file = open('data', 'rb')
     data = load(data_file)
     data_file.close()
 except Exception:
     pass
 
 # Ask user for instance.
-instance = input("Please enter the URL of the instance you would like to " +
-                 "monitor (such as https://mastodon.social/): ")
+instance = input('Please enter the URL of the instance you would like to ' +
+                 'monitor (such as https://mastodon.social/): ')
 
 # Get new data via the API.
-requested_data = ""
+requested_data = ''
 try:
     requested_data = get(instance + 'api/v1/instance/activity').json()
 except exceptions.RequestException:
-    raise SystemExit("Unable to connect to the instance.\nPlease ensure " +
-                     "that the URL is correct (including ending with '/'), " +
-                     "that the instance is **NOT** in whitelist mode, and " +
-                     "that you are connected to the internet.")
+    raise SystemExit('Unable to connect to the instance.\nPlease ensure ' +
+                     'that the URL is correct (including ending with \'/\'), ' +
+                     'that the instance is **NOT** in whitelist mode, and ' +
+                     'that you are connected to the internet.')
 
 unix_timestamp = datetime.timestamp(datetime.now())
 
 while True:
     failed_to_collect = False
     # Get new data via the API.
-    requested_data = ""
-    print("Attempting to collect activity data.\nPlease do NOT terminate " +
-          "during this process.")
+    requested_data = ''
+    print('Attempting to collect activity data.\nPlease do NOT terminate ' +
+          'during this process.')
     try:
         requested_data = get(instance + 'api/v1/instance/activity').json()
     except exceptions.RequestException:
-        print("Unable to collect activity data:", datetime.now())
+        print('Unable to collect activity data:', datetime.now())
         failed_to_collect = True
 
     if not failed_to_collect:
@@ -57,7 +57,7 @@ while True:
             requested_data[requested_data_entry]['week'] = \
                 datetime.fromtimestamp(int(requested_data[requested_data_entry]
                                            ['week'])).strftime('%d/%m/%Y')
-            temp = requested_data[requested_data_entry]['week'].split("/")
+            temp = requested_data[requested_data_entry]['week'].split('/')
             requested_data[requested_data_entry]['week'] = \
                 int(datetime.timestamp(datetime(int(temp[2]), int(temp[1]),
                     int(temp[0]))))
@@ -67,12 +67,12 @@ while True:
                     exists = True
                     break
             if not exists:
-                # Add a "count" key == 1
+                # Add a 'count' key == 1
                 requested_data[requested_data_entry]['count'] = '1'
                 data.append(requested_data[requested_data_entry])
             else:
-                # Sum old and new "statuses", "logins" and "registrations", and
-                # increment "count".
+                # Sum old and new 'statuses', 'logins' and 'registrations', and
+                # increment 'count'.
                 # This is used to handle data inconsistencies between requests.
                 data[data_entry]['statuses'] = \
                     str(int(data[data_entry]['statuses']) +
@@ -93,9 +93,9 @@ while True:
             write_data = open('data', 'wb')
             dump(data, write_data)
             write_data.close()
-            print("Successfully collected activity data:", datetime.now())
+            print('Successfully collected activity data:', datetime.now())
         except Exception:
-            print("Failed to save the collected activity data:",
+            print('Failed to save the collected activity data:',
                   datetime.now())
 
     # Ensures that the program only requests data once every 24 hours.
