418bd4ae9b424f53a3825dd99c7a408f3fdb8517
Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
2024-03-12 16:14:38 +0000

    Add ability to change file name of saved graph
    The user can now enter a file name for the saved graph. A default name
    will be used if this is not provided.
    
    Resolves: #5

--- front-end.py
+++ front-end.py
@@ -19,7 +19,7 @@ matplotlib.use('TkAgg')
 
 
 # Plot the graph.
-def draw_figure(data_df, graph_canvas, root, save):
+def draw_figure(data_df, graph_canvas, root, file_name, save):
     # Remove the old graph.
     graph_canvas.grid_remove()
     graph_canvas = tk.Canvas(root, height=250, width=300)
@@ -69,7 +69,10 @@ def draw_figure(data_df, graph_canvas, root, save):
     figure_canvas_agg.get_tk_widget().grid(row=1, column=0, columnspan=2,
                                            rowspan=2)
     if save:
-        fig.savefig("graph.png")
+        if file_name != '':
+            fig.savefig(file_name + '.png')
+        else:
+            fig.savefig('graph.png')
     return figure_canvas_agg
 
 
@@ -93,12 +96,13 @@ def create_dataframe(data, data_quantity):
 # Get the input from the entries text box.
 def take_entries_input(data, data_quantity, graph_canvas, root, save):
     input_number = entries_text_box.get('1.0', 'end-1c')
+    file_name = save_text_box.get('1.0', 'end-1c')
     if input_number.strip().isdigit():
         if int(input_number.strip()) <= len(data) - 1 \
                 and int(input_number.strip()) > 0:
             data_quantity = int(input_number.strip())
     data_df = create_dataframe(data, data_quantity)
-    draw_figure(data_df, graph_canvas, root, save)
+    draw_figure(data_df, graph_canvas, root, file_name, save)
 
 
 # Get the collected data.
@@ -141,7 +145,7 @@ input_grid = tk.Canvas(root, height=250, width=300)
 input_grid.grid(row=0, column=0, columnspan=2)
 
 # Create the initial graph.
-draw_figure(data_df, graph_canvas, root, False)
+draw_figure(data_df, graph_canvas, root, '', False)
 
 # Create the elements for the input_grid canvas.
 entries_label = tk.Label(input_grid,
@@ -152,14 +156,23 @@ entries_button = tk.Button(input_grid, height=1, width=20, text='Enter',
                            command=lambda:
                            take_entries_input(data, data_quantity,
                                               graph_canvas, root, False))
+
+save_label = tk.Label(input_grid,
+                      text='Enter the file name you want the gaph to be ' +
+                           'saved as:', anchor='sw')
+save_text_box = tk.Text(input_grid, height=1, pady=5)
 save_button = tk.Button(input_grid, height=1, width=20, text='Save graph',
-                           command=lambda:
-                           take_entries_input(data, data_quantity,
-                                              graph_canvas, root, True))
+                        command=lambda:
+                        take_entries_input(data, data_quantity,
+                                           graph_canvas, root, True))
+
 entries_label.grid(row=0, column=0, sticky='sw')
 entries_text_box.grid(row=1, column=0, sticky='w')
 entries_button.grid(row=1, column=1)
-save_button.grid(row=0, column=1)
+
+save_label.grid(row=2, column=0, sticky='sw')
+save_text_box.grid(row=3, column=0, sticky='w')
+save_button.grid(row=3, column=1)
 
 # Ensure that the elements in the window scale appropriately.
 root.grid_columnconfigure(0, weight=1)
