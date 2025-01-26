4354f563b417d46ad9ff9a27719c788a9a77179c
Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
2025-01-26 10:37:28 +0000

    Add ability to choose graph dates
    The user can now choose a start and end date for the data they would
    like to be displayed on the graph.
    
    Resolves: #6

--- front-end.py
+++ front-end.py
@@ -14,6 +14,8 @@ import pandas as pd
 import tkinter as tk
 from sys import exit
 from datetime import datetime
+import time
+import tkcalendar
 
 matplotlib.use('TkAgg')
 
@@ -81,37 +83,41 @@ def draw_figure(data_df, root, file_name, save):
 # Ask for confirmation before closing the program.
 def on_closing():
     if tk.messagebox.askokcancel('Quit', 'Do you want to quit?'):
-        root.destroy()
         exit()
 
 
 # Create the DataFrame.
-def create_dataframe(data, data_quantity):
+def create_dataframe(data):
     data_df = pd.DataFrame(data)
     data_df = data_df.sort_values(by=['week'], ascending=False)
     data_df = data_df.reset_index()
     data_df = data_df.drop('index', axis=1)
-    data_df = data_df.head(data_quantity)
     return data_df
 
 
 # Get the inputs from the entries text box.
-def get_inputs(data, data_quantity, root, save):
-    input_number = entries_text_box.get('1.0', 'end-1c')
+def get_inputs(data, root, save):
+    # Get the file name.
     file_name = save_text_box.get('1.0', 'end-1c')
-    if input_number.strip().isdigit():
-        if int(input_number.strip()) <= len(data) - 1 \
-                and int(input_number.strip()) > 0:
-            data_quantity.set(int(input_number.strip()))
-    data_df = create_dataframe(data, data_quantity.get())
+    # Create a new DataFrame which only contains the data between midnight of
+    # the earlier date and just before the end of the later date.
+    data_df = create_dataframe(data)
+    limit1 = int(time.mktime(time.strptime(date1.get(), '%d/%m/%Y')))
+    limit2 = int(time.mktime(time.strptime(date2.get(), '%d/%m/%Y')))
+    if limit1 > limit2:
+        temp = limit1
+        limit1 = limit2
+        limit2 = temp
+    limit2 += (24 * 60 * 60)
+    data_df = data_df[data_df['week'].astype(int) >= limit1]
+    data_df = data_df[data_df['week'].astype(int) < limit2]
     draw_figure(data_df, root, file_name, save)
 
-test = 0
 # Create the window.
 root = tk.Tk()
 root.title('Charting Mastodon Activity')
 
-# Set the 'x' on the windoiw to call the on_closing function.
+# Set the 'x' on the window to call the on_closing function.
 root.protocol('WM_DELETE_WINDOW', on_closing)
 
 # Create the reuired variables for the checkboxes.
@@ -130,22 +136,22 @@ try:
 except Exception:
     pass
 
-# Create a DataFrame to set an initial value for data_quantity.
-data_quantity = tk.IntVar()
+# Create a DataFrame to set a value for how much data to show initially.
 data_df = pd.DataFrame(data)
 data_df = data_df.sort_values(by=['week'], ascending=False)
 data_df = data_df.reset_index()
 data_df = data_df.drop('index', axis=1)
 if len(data_df) < 12:
-    data_quantity.set(len(data_df))
+    data_quantity = len(data_df)
 else:
-    data_quantity.set(12)
+    data_quantity = 12
 
 # Create the inital DataFrame using the data_quantity limit.
-data_df = create_dataframe(data, data_quantity.get())
+data_df = create_dataframe(data)
+data_df = data_df.head(data_quantity)
 
-# Create the canvas for getting inputs from the user.
-input_grid = tk.Canvas(root, height=250, width=300)
+# Create the frame for getting inputs from the user.
+input_grid = tk.Frame(root, height=250, width=300)
 input_grid.grid(row=0, column=0, columnspan=2, rowspan=2)
 
 # Create the initial graph.
@@ -185,34 +191,37 @@ for x in range(0, len(old_labels)):
 ax.set_xticks(old_labels)
 ax.set_xticklabels(labels, rotation=15)
 
-# Create the elements for the input_grid canvas.
+# Create the elements for the input_grid frame.
 entries_label = tk.Label(input_grid,
-                         text='Enter number of data entries to plot:',
+                         text='Enter the start and end dates for the data:',
                          width=37, anchor='sw')
-entries_text_box = tk.Text(input_grid, height=1, pady=5)
+
 entries_button = tk.Button(input_grid, height=6, width=20, text='Enter',
                            command=lambda:
-                           get_inputs(data, data_quantity,
-                                      root, False))
+                           get_inputs(data, root, False))
 
 save_label = tk.Label(input_grid,
-                      text='\nEnter the file name you want the gaph to be ' +
+                      text='\nEnter the file name you want the graph to be ' +
                            'saved as:', anchor='sw')
 save_text_box = tk.Text(input_grid, height=1, pady=5)
 save_button = tk.Button(input_grid, height=1, width=20, text='Save graph',
                         command=lambda:
-                        get_inputs(data, data_quantity,
-                                   root, True))
+                        get_inputs(data, root, True))
 
-# Create the canvas for the checkboxes.
-checkbox_grid = tk.Canvas(input_grid, height=250, width=300)
+# Create the frame for the start and end date selection.
+dates_grid = tk.Frame(input_grid, height=50, width=300)
 
-# Create the canvas for the width and height inputs.
-graph_size_grid = tk.Canvas(input_grid, height=250, width=300)
+# Create the frame for the checkboxes.
+checkbox_grid = tk.Frame(input_grid, height=50, width=300)
 
-# Add the elements to the input_grid canvas.
+# Create the frames for the width and height inputs.
+graph_size_grid = tk.Frame(input_grid, height=50, width=300)
+
+# Add the elements to the input_grid frame.
 entries_label.grid(row=0, column=0, sticky='sw')
-entries_text_box.grid(row=1, column=0, sticky='w')
+
+dates_grid.grid(row=1, column=0, sticky='w')
+
 entries_button.grid(row=1, column=1, rowspan=4)
 
 checkbox_grid.grid(row=2, column=0, sticky='n')
@@ -238,25 +247,35 @@ registrations_checkbox = tk.Checkbutton(checkbox_grid,
                                         onvalue=True, offvalue=False)
 registrations_checkbox.select()
 
-# Add the checkboxes to the checkbox_grid canvas.
+# Add the checkboxes to the checkbox_grid frame.
 statuses_checkbox.grid(row=0, column=0)
 logins_checkbox.grid(row=0, column=1)
 registrations_checkbox.grid(row=0, column=2)
 
-# Create the elements for the graph_size_grid canvas.
-width_label = tk.Label(graph_size_grid,
-                      text='Enter the width of the graph:', anchor='sw')
+# Create the elements for the graph_size_grid frame.
+width_label = tk.Label(graph_size_grid, text='Enter the width of the graph in inches (optional):')
 width_text_box = tk.Text(graph_size_grid, height=1, width=39, pady=5)
-
-height_label = tk.Label(graph_size_grid,
-                      text='Enter the height of the graph:', anchor='sw')
+height_label = tk.Label(graph_size_grid, text='Enter the height of the graph in inches (optional):')
 height_text_box = tk.Text(graph_size_grid, height=1, width=40, pady=5)
 
-# Add the checkboxes to the graph_size_grid canvas.
+# Add the checkboxes to the graph_size_grid frame.
 width_label.grid(row=0, column=0)
 width_text_box.grid(row=1, column=0)
 height_label.grid(row=0, column=1)
 height_text_box.grid(row=1, column=1)
+
+# Create the calendars for the dates_grid frame.
+start_date = datetime.fromtimestamp(float(old_labels[0])).strftime('%d/%m/%Y')
+date1 = tkcalendar.DateEntry(dates_grid, width=38)
+date1.set_date(start_date)
+
+end_date = datetime.fromtimestamp(float(old_labels[len(old_labels)-1])).strftime('%d/%m/%Y')
+date2 = tkcalendar.DateEntry(dates_grid, width=39)
+date2.set_date(end_date)
+
+# Add the calendars to the dates_grid frame.
+date1.grid(row=0, column=0)
+date2.grid(row=0, column=1)
 
 # Ensure that the elements in the window scale appropriately.
 root.grid_columnconfigure(0, weight=1)
