797e0f8141bbc923062c4d2c305400ea266d353c
Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
2025-02-01 16:17:09 +0000

    Squashed commit of the following:
    commit 62e4b9841d493055501540753b0e1042e593cce4
    Author: Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
    Date:   Sat Feb 1 16:15:52 2025 +0000
    
        Update README.md
    
        The python version stated has been updated.
    
    commit f4aaf03fea7ddbd4c8b48815964c46b74b1a35c7
    Author: Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
    Date:   Sat Feb 1 16:10:06 2025 +0000
    
        Make front-end PEP8 compliant
    
        Refactored the front-end so that it is PEP8 compliant. This has been
        checked using pycodestyle.
    
    commit beea150229210ae1e2140e23ba301d7f9017499a
    Author: Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
    Date:   Sat Feb 1 14:06:53 2025 +0000
    
        Add scroll bars
    
        The user can now use the scroll bars to view everything displayed when
        the content doesn't fit inside the window.
    
    commit 4354f563b417d46ad9ff9a27719c788a9a77179c
    Author: Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
    Date:   Sun Jan 26 10:37:28 2025 +0000
    
        Add ability to choose graph dates
    
        The user can now choose a start and end date for the data they would
        like to be displayed on the graph.
    
        Resolves: #6
    
    commit 395085617e5cfe64920c23e58c68498bea8fb9e5
    Author: Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
    Date:   Sun Jan 26 10:24:32 2025 +0000
    
        Update requirements.txt
    
    commit 83c4674405e62da8d4f49d27466376c683d5ce9c
    Author: Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
    Date:   Fri Mar 15 12:25:26 2024 +0000
    
        Add ability to resize the graph
    
        The user can now input a height and width for the graph. This will
        change these dimensions for the graph shown in the window and the
        resulting image of the saved graph.
    
        Resolves: #12
    
    commit a6ffa1647d4001dedf191310cf38209d9a9be2fa
    Author: Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
    Date:   Thu Mar 14 15:24:19 2024 +0000
    
        Add ability to hide metrics
    
        A checkbox has been added for each metric, and the user can decide
        which to show on the graph and which to hide. The code is also PEP8
        compliant.
    
        Resolves: #4
    
    commit 3cdb428a17075ba680a3a6679599529b867f5531
    Author: Ubaydullah-A <157973965+Ubaydullah-A@users.noreply.github.com>
    Date:   Thu Mar 14 12:53:04 2024 +0000
    
        Edit code to allow for graph customisation
    
        The code has been reordered to allow for graph customisation. Also, a
        checkbox has been added that will be used later, but currently only
        changes a value that is printed when a graph is created. The code is
        currently not PEP8 compliant.

--- front-end.py
+++ front-end.py
@@ -14,43 +14,55 @@ import pandas as pd
 import tkinter as tk
 from sys import exit
 from datetime import datetime
+import time
+import tkcalendar
+from tkinter import ttk
 
 matplotlib.use('TkAgg')
 
 
 # Plot the graph.
-def draw_figure(data_df, graph_canvas, root, file_name, save):
+def draw_figure(data_df, frame, file_name, save):
     # Remove the old graph.
-    graph_canvas.grid_remove()
-    graph_canvas = tk.Canvas(root, height=250, width=300)
-    graph_canvas.grid(row=1, column=0, columnspan=2, rowspan=2)
+    global figure_canvas_agg
+    figure_canvas_agg.get_tk_widget().destroy()
+    plt.close('all')
 
     # Create the new graph.
     fig, ax = plt.subplots()
     ax.grid()
+    width_value = width_text_box.get('1.0', 'end-1c').strip()
+    height_value = height_text_box.get('1.0', 'end-1c').strip()
+    if width_value.isdigit():
+        fig.set_figwidth(int(width_value))
+    if height_value.isdigit():
+        fig.set_figheight(int(height_value))
 
     # Plot the data on the graph.
-    ax.plot(data_df['week'].to_numpy(),
-            data_df['statuses'].to_numpy().astype(int) /
-            data_df['count'].to_numpy().astype(int),
-            label='statuses', marker='x')
-    ax.plot(data_df['week'].to_numpy(),
-            data_df['logins'].to_numpy().astype(int) /
-            data_df['count'].to_numpy().astype(int),
-            label='logins', marker='x')
-    ax.plot(data_df['week'].to_numpy(),
-            data_df['registrations'].to_numpy().astype(int) /
-            data_df['count'].to_numpy().astype(int),
-            label='registrations', marker='x')
+    if show_statuses.get():
+        ax.plot(data_df['week'].to_numpy(),
+                data_df['statuses'].to_numpy().astype(int) /
+                data_df['count'].to_numpy().astype(int),
+                label='statuses', marker='x')
+    if show_logins.get():
+        ax.plot(data_df['week'].to_numpy(),
+                data_df['logins'].to_numpy().astype(int) /
+                data_df['count'].to_numpy().astype(int),
+                label='logins', marker='x')
+    if show_registrations.get():
+        ax.plot(data_df['week'].to_numpy(),
+                data_df['registrations'].to_numpy().astype(int) /
+                data_df['count'].to_numpy().astype(int),
+                label='registrations', marker='x')
 
     # Create a graph legend.
-    ax.legend(loc='best')
+    if show_statuses.get() or show_logins.get() or show_registrations.get():
+        ax.legend(loc='best')
 
     # Draw the graph in order to get the x-axis labels.
-    figure_canvas_agg = FigureCanvasTkAgg(fig, graph_canvas)
+    figure_canvas_agg = FigureCanvasTkAgg(fig, frame)
     figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().grid(row=1, column=0, columnspan=2,
-                                           rowspan=2)
+    figure_canvas_agg.get_tk_widget().grid(row=2, column=0)
 
     # Replace the x-axis labels with dates rather than Unix timestamps.
     old_labels = [item.get_position() for item in ax.get_xticklabels()]
@@ -62,48 +74,92 @@ def draw_figure(data_df, graph_canvas, root, file_name, save):
         old_labels[x] = float(old_labels[x][0])
     ax.set_xticks(old_labels)
     ax.set_xticklabels(labels, rotation=15)
-
-    # Draw the final graph.
-    figure_canvas_agg = FigureCanvasTkAgg(fig, graph_canvas)
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().grid(row=1, column=0, columnspan=2,
-                                           rowspan=2)
     if save:
         if file_name != '':
             fig.savefig(file_name + '.png')
         else:
             fig.savefig('graph.png')
-    return figure_canvas_agg
 
 
 # Ask for confirmation before closing the program.
 def on_closing():
     if tk.messagebox.askokcancel('Quit', 'Do you want to quit?'):
-        root.destroy()
         exit()
 
 
 # Create the DataFrame.
-def create_dataframe(data, data_quantity):
+def create_dataframe(data):
     data_df = pd.DataFrame(data)
     data_df = data_df.sort_values(by=['week'], ascending=False)
     data_df = data_df.reset_index()
     data_df = data_df.drop('index', axis=1)
-    data_df = data_df.head(data_quantity)
     return data_df
 
 
-# Get the input from the entries text box.
-def take_entries_input(data, data_quantity, graph_canvas, root, save):
-    input_number = entries_text_box.get('1.0', 'end-1c')
+# Get the inputs from the entries text box.
+def get_inputs(data, frame, save):
+    # Get the file name.
     file_name = save_text_box.get('1.0', 'end-1c')
-    if input_number.strip().isdigit():
-        if int(input_number.strip()) <= len(data) - 1 \
-                and int(input_number.strip()) > 0:
-            data_quantity = int(input_number.strip())
-    data_df = create_dataframe(data, data_quantity)
-    draw_figure(data_df, graph_canvas, root, file_name, save)
+    # Create a new DataFrame which only contains the data between midnight of
+    # the earlier date and just before the end of the later date.
+    data_df = create_dataframe(data)
+    limit1 = int(time.mktime(time.strptime(date1.get(), '%d/%m/%Y')))
+    limit2 = int(time.mktime(time.strptime(date2.get(), '%d/%m/%Y')))
+    if limit1 > limit2:
+        temp = limit1
+        limit1 = limit2
+        limit2 = temp
+    limit2 += (24 * 60 * 60)
+    data_df = data_df[data_df['week'].astype(int) >= limit1]
+    data_df = data_df[data_df['week'].astype(int) < limit2]
+    draw_figure(data_df, frame, file_name, save)
 
+
+# Adjust the region that can be scrolled when the information dislayed on the
+# window is changed.
+def on_frame_configure(event):
+    canvas.configure(scrollregion=canvas.bbox('all'))
+
+
+# Resize the canvas to the correct size when the window changes size.
+def resize_canvas(event):
+    canvas.config(width=frame.winfo_width(), height=frame.winfo_height())
+
+
+# Create the window.
+root = tk.Tk()
+root.title('Charting Mastodon Activity')
+
+# Set the 'x' on the window to call the on_closing function.
+root.protocol('WM_DELETE_WINDOW', on_closing)
+
+# Create the canvas for the window.
+canvas = tk.Canvas(root, highlightthickness=0)
+canvas.grid(row=0, column=0, sticky='n')
+
+# Create the frame for grouping the other widgets together.
+frame = tk.Frame(canvas)
+canvas.create_window((0, 0), window=frame)
+
+# Create the scroll bars.
+vertical_scroll_bar = ttk.Scrollbar(root, orient='vertical',
+                                    command=canvas.yview)
+vertical_scroll_bar.grid(row=0, column=1, sticky='ns')
+
+horizontal_scroll_bar = ttk.Scrollbar(root, orient='horizontal',
+                                      command=canvas.xview)
+horizontal_scroll_bar.grid(row=1, column=0, sticky='ew')
+
+# Configure the canvas to allow for scrolling.
+canvas.configure(yscrollcommand=vertical_scroll_bar.set,
+                 xscrollcommand=horizontal_scroll_bar.set)
+
+frame.bind('<Configure>', on_frame_configure)
+
+# Create the required variables for the checkboxes.
+show_statuses = tk.BooleanVar()
+show_logins = tk.BooleanVar()
+show_registrations = tk.BooleanVar()
 
 # Get the collected data.
 file_check = open('data', 'a')
@@ -116,7 +172,7 @@ try:
 except Exception:
     pass
 
-# Create a DataFrame to set an initial value for data_quantity.
+# Create a DataFrame to set a value for how much data to show initially.
 data_df = pd.DataFrame(data)
 data_df = data_df.sort_values(by=['week'], ascending=False)
 data_df = data_df.reset_index()
@@ -127,56 +183,150 @@ else:
     data_quantity = 12
 
 # Create the inital DataFrame using the data_quantity limit.
-data_df = create_dataframe(data, data_quantity)
+data_df = create_dataframe(data)
+data_df = data_df.head(data_quantity)
 
-# Create the window.
-root = tk.Tk()
-root.title('Charting Mastodon Activity')
-
-# Set the 'x' on the windoiw to call the on_closing function.
-root.protocol('WM_DELETE_WINDOW', on_closing)
-
-# Create the initial canvas that the graph will be displayed on.
-graph_canvas = tk.Canvas(root, height=250, width=300)
-graph_canvas.grid(row=2, column=0, columnspan=2, rowspan=2)
-
-# Create the canvas for getting inputs from the user.
-input_grid = tk.Canvas(root, height=250, width=300)
-input_grid.grid(row=0, column=0, columnspan=2)
+# Create the frame for getting inputs from the user.
+input_grid = tk.Frame(frame, height=250, width=300)
+input_grid.grid(row=0, column=0)
 
 # Create the initial graph.
-draw_figure(data_df, graph_canvas, root, '', False)
+fig, ax = plt.subplots()
+ax.grid()
 
-# Create the elements for the input_grid canvas.
+# Plot the data on the graph.
+ax.plot(data_df['week'].to_numpy(),
+        data_df['statuses'].to_numpy().astype(int) /
+        data_df['count'].to_numpy().astype(int),
+        label='statuses', marker='x')
+ax.plot(data_df['week'].to_numpy(),
+        data_df['logins'].to_numpy().astype(int) /
+        data_df['count'].to_numpy().astype(int),
+        label='logins', marker='x')
+ax.plot(data_df['week'].to_numpy(),
+        data_df['registrations'].to_numpy().astype(int) /
+        data_df['count'].to_numpy().astype(int),
+        label='registrations', marker='x')
+
+# Create a graph legend.
+ax.legend(loc='best')
+
+# Draw the graph in order to get the x-axis labels.
+figure_canvas_agg = FigureCanvasTkAgg(fig, frame)
+figure_canvas_agg.draw()
+figure_canvas_agg.get_tk_widget().grid(row=2, column=0)
+
+# Replace the x-axis labels with dates rather than Unix timestamps.
+old_labels = [item.get_position() for item in ax.get_xticklabels()]
+labels = old_labels.copy()
+for label in range(0, len(labels)):
+    labels[label] = datetime.fromtimestamp(
+            float(labels[label][0])).strftime('%d/%m/%y, %H:%M')
+for x in range(0, len(old_labels)):
+    old_labels[x] = float(old_labels[x][0])
+ax.set_xticks(old_labels)
+ax.set_xticklabels(labels, rotation=15)
+
+# Create the elements for the input_grid frame.
 entries_label = tk.Label(input_grid,
-                         text='Enter number of data entries to plot:',
+                         text='Enter the start and end dates for the data:',
                          width=37, anchor='sw')
-entries_text_box = tk.Text(input_grid, height=1, pady=5)
-entries_button = tk.Button(input_grid, height=1, width=20, text='Enter',
+
+entries_button = tk.Button(input_grid, height=6, width=20, text='Enter',
                            command=lambda:
-                           take_entries_input(data, data_quantity,
-                                              graph_canvas, root, False))
+                           get_inputs(data, frame, False))
 
 save_label = tk.Label(input_grid,
-                      text='Enter the file name you want the gaph to be ' +
+                      text='\nEnter the file name you want the graph to be ' +
                            'saved as:', anchor='sw')
 save_text_box = tk.Text(input_grid, height=1, pady=5)
 save_button = tk.Button(input_grid, height=1, width=20, text='Save graph',
                         command=lambda:
-                        take_entries_input(data, data_quantity,
-                                           graph_canvas, root, True))
+                        get_inputs(data, frame, True))
 
+# Create the frame for the start and end date selection.
+dates_grid = tk.Frame(input_grid, height=50, width=300)
+
+# Create the frame for the checkboxes.
+checkbox_grid = tk.Frame(input_grid, height=50, width=300)
+
+# Create the frames for the width and height inputs.
+graph_size_grid = tk.Frame(input_grid, height=50, width=300)
+
+# Add the elements to the input_grid frame.
 entries_label.grid(row=0, column=0, sticky='sw')
-entries_text_box.grid(row=1, column=0, sticky='w')
-entries_button.grid(row=1, column=1)
 
-save_label.grid(row=2, column=0, sticky='sw')
-save_text_box.grid(row=3, column=0, sticky='w')
-save_button.grid(row=3, column=1)
+dates_grid.grid(row=1, column=0, sticky='w')
+
+entries_button.grid(row=1, column=1, rowspan=4)
+
+checkbox_grid.grid(row=2, column=0, sticky='n')
+
+graph_size_grid.grid(row=3, column=0)
+
+save_label.grid(row=5, column=0, sticky='sw')
+save_text_box.grid(row=6, column=0, sticky='w')
+save_button.grid(row=6, column=1)
+
+# Create the checkboxes that will determine if a metric is shown.
+statuses_checkbox = tk.Checkbutton(checkbox_grid, text='Show statuses',
+                                   variable=show_statuses, onvalue=True,
+                                   offvalue=False)
+statuses_checkbox.select()
+logins_checkbox = tk.Checkbutton(checkbox_grid, text='Show logins',
+                                 variable=show_logins, onvalue=True,
+                                 offvalue=False)
+logins_checkbox.select()
+registrations_checkbox = tk.Checkbutton(checkbox_grid,
+                                        text='Show registrations',
+                                        variable=show_registrations,
+                                        onvalue=True, offvalue=False)
+registrations_checkbox.select()
+
+# Add the checkboxes to the checkbox_grid frame.
+statuses_checkbox.grid(row=0, column=0)
+logins_checkbox.grid(row=0, column=1)
+registrations_checkbox.grid(row=0, column=2)
+
+# Create the elements for the graph_size_grid frame.
+width_label = tk.Label(graph_size_grid, text='Enter the width of the graph ' +
+                       'in inches (optional):')
+width_text_box = tk.Text(graph_size_grid, height=1, width=39, pady=5)
+height_label = tk.Label(graph_size_grid, text='Enter the height of the graph' +
+                        ' in inches (optional):')
+height_text_box = tk.Text(graph_size_grid, height=1, width=40, pady=5)
+
+# Add the checkboxes to the graph_size_grid frame.
+width_label.grid(row=0, column=0)
+width_text_box.grid(row=1, column=0)
+height_label.grid(row=0, column=1)
+height_text_box.grid(row=1, column=1)
+
+# Create the calendars for the dates_grid frame.
+start_date = datetime.fromtimestamp(float(old_labels[0])).strftime('%d/%m/%Y')
+date1 = tkcalendar.DateEntry(dates_grid, width=38)
+date1.set_date(start_date)
+
+end_date = datetime.fromtimestamp(float(
+                                  old_labels[len(
+                                    old_labels)-1])).strftime('%d/%m/%Y')
+date2 = tkcalendar.DateEntry(dates_grid, width=39)
+date2.set_date(end_date)
+
+# Add the calendars to the dates_grid frame.
+date1.grid(row=0, column=0)
+date2.grid(row=0, column=1)
 
 # Ensure that the elements in the window scale appropriately.
 root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
-root.grid_rowconfigure(1, weight=1)
+
+# Set the initial window size.
+starting_width = frame.winfo_width()+16
+starting_height = frame.winfo_height()+16
+root.geometry(f'{starting_width}x{starting_height}')
+
+# Call the resize_canvas function when the window changes size.
+root.bind('<Configure>', resize_canvas)
 
 root.mainloop()
